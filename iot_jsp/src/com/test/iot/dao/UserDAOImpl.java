a43b7c31261e1ef9c478d88de0c93936925e3bf9
kmjy <jae080004@gmail.com>
2017-12-21 15:10:39 +0900

    12.21

---
+++ iot_jsp/src/com/test/iot/dao/UserDAOImpl.java
@@ -0,0 +1,20 @@
+package com.test.iot.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserDAOImpl implements UserDAO{
+
+	@Override
+	public ArrayList<HashMap<String, Object>> selectUserList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, Object> selectUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ iot_jsp/src/com/test/iot/service/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.test.iot.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserServiceImpl implements UserService {   //1 콘쉽오 해주기
+
+	@Override
+	public ArrayList<HashMap<String, Object>> getUserList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, Object> getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
